ee32c9572caa55a61362e33f5501b546015119f8
mBentaifour <bentaifourmoh@gmail.com>
2024-10-03 18:44:03 -0700

    ES6 classes JS

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,15 +1,13 @@
 /**
- * Implement a class named ClassRoom
- */
+* Implement a class named ClassRoom
+*/
 
 export default class ClassRoom {
-
-  /**
-   * Creates @see {@link ClassRoom}
-   * @parametre {Number} maxStudentsSize - max size
-   */
-
-  constructor(maxStudentsSize) {
+/**
+* Creates @see {@link ClassRoom}
+* @parametre {Number} maxStudentsSize - max size
+*/
+	constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
 }
