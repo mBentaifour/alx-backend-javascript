51996a9dbd302b0b183b3d990b05deaaef049aa5
mBentaifour <bentaifourmoh@gmail.com>
2024-10-04 17:48:12 -0700

    ES6 Basic JS

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
-module.exports = function taskBlock(trueOrFalse) {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true; /* no-unused-vars */
+    const task2 = false; /* no-unused-vars */
   }
 
   return [task, task2];
