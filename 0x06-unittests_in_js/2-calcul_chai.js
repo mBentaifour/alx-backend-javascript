086d51505e8e6b57f8587795af8698d7463ceb75
mbentaifour <bentaifourmoh@gmail.com>
2024-11-29 13:56:45 +0000

    unittests in JS

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,24 @@
+// 2-calcul_chai.js
+// Rewrite the test suite, using expect from Chai
+
+function calculateNumber(type, a, b) {
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return roundedA + roundedB;
+    case 'SUBTRACT':
+      return roundedA - roundedB;
+    case 'DIVIDE':
+      if (roundedB === 0) {
+        return 'Error';
+      }
+      return roundedA / roundedB;
+    default:
+      throw new Error('Invalid operation type');
+  }
+}
+
+module.exports = calculateNumber;
+
